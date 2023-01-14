69a8b8d96c0dfdc4079a983d3fac6202244d781b
Nathan Soares Silva <nathan.silva@teros.com.br>
2023-01-14 18:14:40 -0300

    site inicial

--- src/components/basic/header/index.tsx
+++ src/components/basic/header/index.tsx
@@ -1,7 +1,6 @@
 import { Box, Button, Drawer } from "@mui/material";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { Cart } from "../../Cart";
 
 export default function Header() {
   const [open, setOpen] = useState<boolean>(false);
@@ -23,76 +22,45 @@ export default function Header() {
         </Box>
       </Drawer>
       <div className="flex items-center flex-shrink-0 text-white ">
-        <a
-          className="text-black no-underline hover:text-zinc-800 hover:no-underline justify-center items-center mr-6"
-          href="#"
-        >
+        <a className="text-black no-underline hover:text-zinc-800 hover:no-underline justify-center items-center mr-6" href="#">
           <div className="flex w-full h-12 justify-center items-center">
-            <img
-              src="/images/logo/logo canunc.png"
-              className="flex object-contain w-full h-full"
-            />
-            <h3 className="font-bold text-4xl ml-3">CANUC</h3>
+            <img src="/images/logo/logo canunc.png" className="flex object-contain w-full h-full" />
+            <h3 className="font-bold text-4xl ml-3">CANUK</h3>
           </div>
         </a>
       </div>
 
       <div className="flex w-1/4 lg:hidden">
-        {showCartButton && <Cart />}
-        <Button
-          id="nav-toggle"
-          onClick={() => setOpen(true)}
-          className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white"
-        >
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+        <Button id="nav-toggle" onClick={() => setOpen(true)} className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white">
+          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <title>Menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
           </svg>
         </Button>
       </div>
 
-      <div
-        className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden pt-6 lg:pt-0"
-        id="nav-content"
-      >
+      <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden pt-6 lg:pt-0" id="nav-content">
         <ul className="list-reset lg:flex justify-end flex-1 items-center">
           <li className="mr-3">
-            <a
-              className="inline-block py-2 px-4 text-black no-underline"
-              href="#"
-            >
+            <a className="inline-block py-2 px-4 text-black no-underline" href="#">
               Home
             </a>
           </li>
           <li className="mr-3">
-            <a
-              className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
-              href="#"
-            >
+            <a className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4" href="#">
               link
             </a>
           </li>
           <li className="mr-3">
-            <a
-              className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
-              href="#"
-            >
+            <a className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4" href="#">
               link
             </a>
           </li>
           <li className="mr-3">
-            <a
-              className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
-              href="#"
-            >
+            <a className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4" href="#">
               link
             </a>
           </li>
-          <li>{showCartButton && <Cart />}</li>
         </ul>
       </div>
     </nav>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { CartContextProvider } from "../contexts/CartContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <CartContextProvider>
-      <Component {...pageProps} />
-    </CartContextProvider>
-  );
+  return <Component {...pageProps} />;
 }
